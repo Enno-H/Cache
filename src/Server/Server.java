81cd72e458ef417cd1649717d1a5d1a7abb221ef
Xiaolong Huang <xiaolong_huang@foxmail.com>
2018-09-28 16:27:38 +1200

    Server(Part 2): add inner class FileFragmenter

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -4,9 +4,10 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 import java.util.logging.Logger;
 
 public class Server {
@@ -150,6 +151,85 @@ public class Server {
             log.info("add file: "+ file.getName());
             fileList.put(file.getName(), file);
         }
+    }
+
+    //内部类
+    class FileFragmenter{
+
+        private  Logger log = Logger.getLogger(FileFragmenter.class.getName());
+        //private Integer prime = 10000169;
+        private Integer prime = 1009;
+        private Integer windowSize = 48;
+        private Integer hashFactor = 256;
+
+        public List<byte[]> fragment(String fileName) {
+            List<byte[]> resultList = new ArrayList<byte[]>();
+            int eliminationMultiplier = generateEliminationMultiplier(hashFactor, windowSize, prime);
+
+            try {
+                Path requestedFile = Paths.get(fileName);
+                byte[] data = Files.readAllBytes(requestedFile);
+                if(data.length <= 2048){
+                    resultList.add(data);
+                    return resultList;
+                }
+                int rollingHash = 0;
+                int boundary = 0;
+                int count = 1;
+                int lastBoundaryIndex = 0;
+                Queue<Integer> window = new LinkedList<Integer>();
+
+                log.info("total bytes : " + data.length);
+                for (int i = 0; i < windowSize; i++) {
+                    int value = data[i];
+                    window.add(value);
+                    rollingHash = (rollingHash * hashFactor + value  + prime) % prime;
+                }
+
+                for (int j = windowSize;  j < data.length; j++)
+                {
+                    int nextValue = data[j];
+                    int firstValueInWindow = window.poll();
+                    window.add(nextValue);
+                    rollingHash = (rollingHash  + prime -  (firstValueInWindow) * (eliminationMultiplier ) % prime) % prime ;
+                    rollingHash = (rollingHash * hashFactor + nextValue ) % prime;
+                    if (rollingHash == boundary){
+                        //log.info("boundary found " + j);
+                        if( j - lastBoundaryIndex >= 2048){
+                            resultList.add(Arrays.copyOfRange(data,lastBoundaryIndex, j));
+                            lastBoundaryIndex = j;
+
+                            count ++;
+                        }
+
+                    }
+
+                }
+                resultList.add(Arrays.copyOfRange(data,lastBoundaryIndex, data.length));
+                log.info("average chunk size " + (data.length / count));
+
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            log.info(resultList.size()+"");
+            return resultList;
+
+        }
+
+
+        private int generateEliminationMultiplier(Integer hashFactor, Integer windowSize, Integer prime) {
+            int multiplier =1;
+            for (int i = 0; i < windowSize -1; i++){
+                multiplier = (multiplier  * hashFactor )% prime;
+            }
+            return multiplier;
+        }
+
+
+
     }
 
 }
